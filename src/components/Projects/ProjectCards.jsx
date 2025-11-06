45876a6a55102ec4948bf7a5489160a3a3e76f54
ibrahimnady <ibrahimnady5@gmail.com>
2025-11-07 01:22:45 +0200

    fix small view

--- src/components/Projects/ProjectCards.jsx
+++ src/components/Projects/ProjectCards.jsx
@@ -14,7 +14,7 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
+        <Button variant="primary" href={props.ghLink} target="_blank" className="ms-md-2 mt-2 mt-md-0">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
@@ -24,13 +24,13 @@ function ProjectCards(props) {
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
         {!props.isBlog && props.demoLink && (
-          <Button variant="primary" href={props.demoLink} target="_blank"  style={{ marginLeft: "10px" }}>
+          <Button variant="primary" href={props.demoLink} target="_blank" style={{ marginLeft: "10px" }} className="ms-md-2 mt-2 mt-md-0">
             <CgWebsite /> &nbsp;
             {"Demo"}
           </Button>
         )}
         {!props.isBlog && props.ReportLink && (
-          <Button variant="primary" href={props.ReportLink} target="_blank"  style={{ marginLeft: "10px" }}>
+          <Button variant="primary" href={props.ReportLink} target="_blank" style={{ marginLeft: "10px" }} className="ms-md-2 mt-2 mt-md-0">
             <TbReportAnalytics /> &nbsp;
             {"Report"}
           </Button>
